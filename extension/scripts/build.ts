9a9b32e33ec0ca3e07c102746ee1ff6651694d1f
TheLazySquid <thelazysquid2@gmail.com>
2025-04-17 17:31:58 -0400

    Clear whole build each time

--- extension/scripts/build.ts
+++ extension/scripts/build.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
 
 // firefox or chrome
-let type = process.argv[2];
+let isFirefox = process.argv[2] === "firefox";
 
-if(!fs.existsSync("./build")) fs.mkdirSync("./build");
+// clear the build directory
+if(fs.existsSync("./build")) fs.rmSync("./build", { recursive: true });
+fs.mkdirSync("./build");
 
 if(!fs.existsSync("./build/images")) {
     fs.cpSync('./images', './build/images', { recursive: true });
@@ -11,14 +13,14 @@ if(!fs.existsSync("./build/images")) {
 fs.copyFileSync('./edit_csp.json', './build/edit_csp.json');
 fs.copyFileSync('./pages/popup.html', './build/popup.html');
 
-if(type === "firefox") {
+if(isFirefox) {
     fs.copyFileSync('./pages/editor_firefox.html', './build/editor.html');
 } else {
     fs.copyFileSync('./pages/editor_chrome.html', './build/editor.html');
 }
 
 let manifest: any;
-if(type === "firefox") {
+if(isFirefox) {
     manifest = JSON.parse(fs.readFileSync('./manifest.firefox.json').toString());
 } else {
     manifest = JSON.parse(fs.readFileSync('./manifest.chrome.json').toString());
@@ -28,19 +30,3 @@ let pkg = JSON.parse(fs.readFileSync('../package.json').toString());
 manifest.version = pkg.version;
 
 fs.writeFileSync('./build/manifest.json', JSON.stringify(manifest, null, 4));
-
-// remove js/relay when not on firefox
-if(type !== "firefox") {
-    if(fs.existsSync("./build/js/relay")) {
-        fs.rmSync("./build/js/relay", { recursive: true });
-    }
-} else {
-    // remove editor/vs and editor/index.css when on firefox
-    if(fs.existsSync("./build/editor/vs")) {
-        fs.rmSync("./build/editor/vs", { recursive: true });
-    }
-
-    if(fs.existsSync("./build/editor/index.css")) {
-        fs.rmSync("./build/editor/index.css");
-    }
-}
