006b9f5adac921d73baa9c006cfa4dbd6ffa983b
TheLazySquid <thelazysquid2@gmail.com>
2025-05-05 07:41:23 -0400

    Add links to site/repo in popup

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -4,6 +4,8 @@
     import type { PluginInfo } from '$types/state';
     import Port from '$shared/port.svelte';
     import DeleteOutline from 'svelte-material-icons/DeleteOutline.svelte';
+    import Web from 'svelte-material-icons/Web.svelte';
+    import GithubIcon from '$assets/github-mark-white.svg';
 
     function onToggle(plugin: PluginInfo) {
         Port.send("pluginToggled", { name: plugin.name, enabled: plugin.enabled });
@@ -22,13 +24,18 @@
         e.stopPropagation();
         deleting = plugin;
     }
+
+    function openSite() { chrome.tabs.create({ url: "https://gimloader.github.io" }) }
+    function openRepo() { chrome.tabs.create({ url: "https://github.com/Gimloader/Gimloader" })}
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events a11y_no_static_element_interactions -->
 <div class="w-full h-full preflight bg-slate-900 text-white" onclick={() => deleting = null}>
     <div class="flex items-center gap-2 px-1 border-b">
         <img src="./images/icon128.png" alt="The Gimloader icon" class="w-6 h-6" />
-        <h1 class="whitespace-nowrap text-2xl font-bold">Gimloader</h1>
+        <h1 class="whitespace-nowrap text-2xl font-bold flex-grow">Gimloader</h1>
+        <button onclick={openSite} title="Open official site"><Web width={24} height={24} /></button>
+        <button onclick={openRepo} title="Open github repo">{@html GithubIcon}</button>
     </div>
     <div class="max-h-[500px] overflow-y-auto w-full py-1">
         {#if state.plugins.length === 0}
