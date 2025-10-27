172cc8d4e34f00e78a7fd3e8e9eb99f4c61b9e5a
TheLazySquid <thelazysquid2@gmail.com>
2025-10-26 22:30:44 -0400

    Add redirect to help with repo restructuring

--- src/shared/net.ts
+++ src/shared/net.ts
@@ -1,5 +1,10 @@
+const oldPathRegex = /^https:\/\/raw.githubusercontent\.com\/Gimloader\/client-plugins\/(?:refs\/heads\/)?main\/(plugins|libraries)(?:\/\w+\/build)?\/(\w+\.js)$/g;
 export function formatDownloadUrl(url: string) {
-    // to help smooth the switch between repositories
+    // migrate from TheLazySquid/Gimloader -> Gimloader/client-plugins
     url = url.replace("https://raw.githubusercontent.com/TheLazySquid/Gimloader", "https://raw.githubusercontent.com/Gimloader/client-plugins");
+
+    // migrate structure of client-plugins repo
+    url = url.replace(oldPathRegex, "https://raw.githubusercontent.com/Gimloader/client-plugins/main/build/$1/$2");
+
     return url;
 }
