8f3dcf6540df9211829f7e800f42a6ea7eb67019
TheLazySquid <thelazysquid2@gmail.com>
2025-12-21 19:50:46 -0500

    Fix keydowns in menus for real this time

--- src/shared/ui/dialog/dialog-content.svelte
+++ src/shared/ui/dialog/dialog-content.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Dialog as DialogPrimitive } from "bits-ui";
     import XIcon from "@lucide/svelte/icons/x";
-    import type { Snippet } from "svelte";
+    import { onMount, type Snippet } from "svelte";
     import * as Dialog from "./index.js";
     import { cn, type WithoutChildrenOrChild } from "$shared/shadcn.js";
 
@@ -20,10 +20,14 @@
         preflight?: boolean;
     } = $props();
 
-    function onKeydown(e: KeyboardEvent) {
-        if(e.key === "Escape") return;
-        e.stopPropagation();
-    }
+    // For some reason the onkeydown listener stops working if another dialog is opened
+    // So this just attaches it directly and should always work
+    $effect(() => {
+        ref?.addEventListener("keydown", (e) => {
+            if(e.key === "Escape") return;
+            e.stopPropagation();
+        });
+    });
 </script>
 
 <Dialog.Portal {...portalProps}>
@@ -36,7 +40,6 @@
             "flex max-h-[calc(100%-32px)] bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed left-[50%] top-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200",
             className
         )}
-        onkeydowncapture={onKeydown}
         {...restProps}>
         {@render children?.()}
         {#if showCloseButton}
